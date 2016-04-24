7d6d129b185fef86e107227ac2235a82f03f4072
jaytheking <jacques.jamieson@gmail.com>
2016-04-25 02:14:42 +1200

    implemented tempalates

--- app.py
+++ app.py
@@ -6,21 +6,29 @@ import markdown
 app = Flask(__name__)
 
 article_dir = 'articles/'
+print(os.listdir('articles'))
+
+
 
 @app.route('/')
 def index():
-	flist = os.listdir('articles')
-	return render_template('index.html', flist = flist)
+
+    flist = os.listdir('articles')
+    return render_template('index.html', flist=flist, header=True)
+
+
+
 
 @app.route('/a/<file_name>')
 def article(file_name):
-	try:
-		fin = codecs.open(article_dir + file_name, mode='r', encoding='utf-8')
-	except IOError:
-		return abort(404)
-	html = markdown.markdown(fin.read())
-	fin.close()
-	return render_template('article.html', html = html)
+    try:
+        fin = codecs.open(article_dir + file_name, mode='r', encoding='utf-8')
+    except IOError:
+        return abort(404)
+    html = markdown.markdown(fin.read())
+    fin.close()
+    return render_template('article.html', html=html, header=False)
+
 
 if __name__ == '__main__':
-	app.run(debug=False)
+    app.run(debug=True)
