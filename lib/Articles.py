85019372d5e243bee9aedd8588d83d9d9cab20f0
jaytheking <jacques.jamieson@gmail.com>
2016-04-26 01:10:22 +1200

    refactoring paginate

--- lib/Articles.py
+++ lib/Articles.py
@@ -6,26 +6,13 @@ from copy import deepcopy
 class Articles(object):
 
     def __init__(self, articles, per_page):
-        self.articles = deepcopy(articles)
+        self.articles = articles
         self.article_count = len(self.articles)
-        self.articles_paginated = {}
         self.per_page = per_page
-        self.__paginate_articles(self.per_page, self.article_count)
 
-    def get_articles_for_page(self, page, art_count):
-        if self.articles_paginated:
-            return self.articles_paginated["page{0}".format(str(page))]
-        else:
-            self.__paginate_articles(self.per_page, art_count)
-        return self.articles_paginated["page{0}".format(str(page))]
+    @property
+    def pages(self):
+        return int(ceil(self.total_count / float(self.per_page)))
 
-    def __paginate_articles(self, per_page, art_count):
-        local_articles = deepcopy(self.articles)
-        num_pages = int(ceil(art_count / float(per_page)))
-        for page in range(num_pages):
-            page_num = "page{0}".format(str(page))
-            self.articles_paginated[page_num] = []
-            for article in range(per_page):
-                if len(local_articles) > 0:
-                    next_article = local_articles.pop()
-                    self.articles_paginated[page_num].append(next_article)
+    def get_articles_for_page(self, page, per_page, count):
+        pass
